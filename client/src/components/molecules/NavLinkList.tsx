3a39f8d554cae3447ab1a5a4e500f131a48dee42
oleksandrazhyntarev <oazhyntarev@greenstreet.com>
2026-03-22 15:29:39 +0200

    secure links on nav

--- client/src/components/molecules/NavLinkList.tsx
+++ client/src/components/molecules/NavLinkList.tsx
@@ -1,9 +1,12 @@
 import Nav from 'react-bootstrap/Nav';
 import { NavLink as RouterNavLink } from 'react-router-dom';
 
+import { useAppSelector } from 'store/store';
+
 export interface NavLink {
   to: string;
   text: string;
+  private?: boolean;
 }
 
 export interface NavLinkListProps {
@@ -11,13 +14,17 @@ export interface NavLinkListProps {
 }
 
 export function NavLinkList({ items }: NavLinkListProps) {
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Nav className="me-auto">
-      {items.map((item) => (
-        <Nav.Link as={RouterNavLink} key={item.to} to={item.to}>
-          {item.text}
-        </Nav.Link>
-      ))}
+      {items
+        .filter((item) => !item.private || isAuthenticated)
+        .map((item) => (
+          <Nav.Link as={RouterNavLink} key={item.to} to={item.to}>
+            {item.text}
+          </Nav.Link>
+        ))}
     </Nav>
   );
 }
--- client/src/components/organisms/Navigation.tsx
+++ client/src/components/organisms/Navigation.tsx
@@ -4,19 +4,20 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
-import { useAppDispatch } from 'store/store';
+import { useAppDispatch, useAppSelector } from 'store/store';
 import { signOut } from 'modules/auth/service';
 import { NavBrand } from 'components/molecules/NavBrand';
 import { NavLinkList, NavLinkListProps } from 'components/molecules/NavLinkList';
 
 const NAV_ITEMS: NavLinkListProps['items'] = [
-  { to: '/threads', text: 'Threads' },
-  { to: '/search-users', text: 'Search Users' },
+  { to: '/threads', text: 'Threads', private: true },
+  { to: '/search-users', text: 'Search Users', private: true },
 ];
 
 export function Navigation() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
 
   const handleLogout = () => {
     dispatch(signOut()).then(() => navigate('/sign-in'));
@@ -28,9 +29,11 @@ export function Navigation() {
         <NavBrand text="Real Time Chat" />
         <Navbar.Collapse>
           <NavLinkList items={NAV_ITEMS} />
-          <Button variant="outline-danger" onClick={handleLogout}>
-            Logout
-          </Button>
+          {isAuthenticated && (
+            <Button variant="outline-danger" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
