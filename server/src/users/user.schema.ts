e5b29e8ab2d51e19f63c82fbbb7cab3ae0a98b02
oleksandrazhyntarev <oazhyntarev@greenstreet.com>
2026-03-31 09:02:22 +0300

    Remove PASSWORD_MAX_LENGTH constant from user schema validation, simplifying password property definition.

--- server/src/users/user.schema.ts
+++ server/src/users/user.schema.ts
@@ -3,7 +3,6 @@ import { HydratedDocument } from 'mongoose';
 import {
   USERNAME_MAX_LENGTH,
   EMAIL_MAX_LENGTH,
-  PASSWORD_MAX_LENGTH,
 } from '../common/constants/validation.constants';
 
 export type UserDocument = HydratedDocument<User>;
@@ -16,7 +15,7 @@ export class User {
   @Prop({ required: true, maxlength: EMAIL_MAX_LENGTH, unique: true })
   email: string;
 
-  @Prop({ required: true, maxLength: PASSWORD_MAX_LENGTH })
+  @Prop({ required: true })
   password: string;
 }
 
